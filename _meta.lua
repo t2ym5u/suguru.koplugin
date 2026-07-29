ba7c87b9b8460510c0052ba25cfe5f7201162c13
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-29 10:22:15 +0200

    chore: bump version to 1.1.10

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "suguru",
     fullname    = _("Suguru"),
     description = _("Fill regions with non-repeating numbers"),
-    version     = "1.1.9",
+    version     = "1.1.10",
 }
