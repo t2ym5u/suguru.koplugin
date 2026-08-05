71f2e6565b9e5abb5b26cb362ca17d10f0707f00
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 14:59:40 +0200

    feat: add ES and DE translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,4 +1,4 @@
 return {
-    ["Fill regions with non-repeating numbers"] = { fr = "Remplissez les régions avec des chiffres non répétés" },
-    ["Suguru"] = { fr = "Suguru" },
+    ["Fill regions with non-repeating numbers"] = { fr = "Remplissez les régions avec des chiffres non répétés", es = "Rellene las regiones con números sin repetir", de = "Fülle die Regionen mit nicht wiederholenden Zahlen" },
+    ["Suguru"] = { fr = "Suguru", es = "Suguru", de = "Suguru" },
 }
